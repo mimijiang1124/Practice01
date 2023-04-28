35f854d8b408935760fe5b2cec07fbfa035e786b
mimijiang1124 <jtingting1124@gmail.com>
2023-04-28 16:58:50 +0800

    I changed the random algorithm

--- random.java
+++ random.java
@@ -5,31 +5,20 @@ public class random {
 
     static int temp = 0;
     static int userInputLength = 0;
+    static int IDlength = 0;
+    static String specialChars;
+    static String chars;
+    static boolean withSpecialChars = false;
+    static String prefix = "";
+    static String suffix = "";
+    static String newID;
 
     public static void main(String[] args) {
 
         userInputLength = input();
         // String output = "";
+        // Try to understand the code
 
-        for (int i = userInputLength; i > 0; i--) {
-            Random randomString = new Random();
-            System.out.printf("\n\nrandomString ", randomString);
-
-            char randomCharacter = (char) (randomString.nextInt(26) + 'a');
-
-            // System.out.print(randomCharacter);
-
-            Random randomInt = new Random();
-            System.out.printf("\nrandomInt ", randomInt);
-
-            int randomNumber = (int) (randomInt.nextInt(9) + '0');
-            temp = randomNumber;
-            if ((temp % 2) == 0) {
-                System.out.print(temp);
-            } else {
-                System.out.print(randomCharacter);
-            }
-        }
     }
 
     public static int input() {
@@ -39,6 +28,23 @@ public class random {
         length = sc.nextInt();
         sc.close();
         return length;
+    }
+
+    public String uniqueID_generator(int a, boolean b, String c, String d, Random randomInt){
+        specialChars = "@#%@#$@#$^";
+
+        if (b){
+            
+        }else{
+            
+        }
+
+        for (int i=0; i<a; i++){
+            int randomNumber = (int) (randomInt.nextInt(9) + '0');
+            String newID = newID + 
+        }
+
+        return "";
     }
 
 }
